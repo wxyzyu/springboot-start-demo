affbb73ed040d42bd1bf623b1bfdf1fcc18555ee
wxyzyu <wxyzyu@126.com>
2020-04-02 21:12:50 +0800

    spring boot demo初始版本

---
+++ src/test/java/com/wxyzyu/springbootstartdemo/SpringbootStartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxyzyu.springbootstartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
